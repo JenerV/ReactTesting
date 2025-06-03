a48f3329e2ece072ead7d0080a57038788ae844e
JenerV <jevelozam@udistrital.edu.co>
2025-06-03 11:36:08 -0500

    index.js

--- react-test/src/index.js
+++ react-test/src/index.js
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Like from "./components/Like";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Like from './components/Like'; 
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Like />
   </React.StrictMode>
 );
+
+reportWebVitals();
+
