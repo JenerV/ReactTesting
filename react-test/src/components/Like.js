0c08ef4e004b73dc3dd4127745a19f1aa3c3b06d
JenerV <jevelozam@udistrital.edu.co>
2025-06-03 11:34:43 -0500

    Like.js

--- react-test/src/components/Like.js
+++ react-test/src/components/Like.js
@@ -1,27 +1,22 @@
 import { useState } from "react";
 
-function Like(){
-    const [likes, setLikes] = useState(0);
-    const handleIncrement = () => {
-        setLikes(likes + 1)
-    };
-    
-    const handleDecrement = () => {
-        setLikes(likes - 1)
-    };
+function Like() {
+  const [likes, setLikes] = useState(0);
+  
+  const handleIncrement = () => setLikes(likes + 1);
+  const handleDecrement = () => setLikes(likes - 1);
 
-    return (
-        <div>
-          <p>Likes: {this.state.likes}</p>
-          <button id="increment" onClick={handleIncrement}>
-            Like
-          </button>
-          <button id="decrement" onClick={handleDecrement}>
-            {" "}
-            Dislike
-          </button>
-        </div>
-      );
+  return (
+    <div>
+      <p>Likes: {likes}</p>
+      <button id="increment" onClick={handleIncrement}>
+        Like
+      </button>
+      <button id="decrement" onClick={handleDecrement}>
+        Dislike
+      </button>
+    </div>
+  );
 }
 
 export default Like;
